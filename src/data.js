80551d3aa32feeea4dcd45e7b792008b009a645b
Dakshak Nagrale <dakshak1997@rediffmail.com>
2021-02-07 02:11:18 +0530

    Adding proj to Git with gh-pages deployment

---
+++ src/data.js
@@ -0,0 +1,19 @@
+const userList = [
+    {
+        id: 1,
+        name: 'Bruce Wayne',
+        username: 'Batman'
+    },
+    {
+        id: 2,
+        name: 'Clark Kent',
+        username: 'Superman'
+    },
+    {
+        id: 3,
+        name: 'Diana Prince',
+        username: 'Wonder Woman'
+    }
+];
+
+export default userList;
